e9c77ca812e9927e2866017de8cac6fd7e48152c
madan <madanbastakoti5@gmail.com>
2022-08-01 11:03:43 +0545

    adding theproducts and fioltering it

---
+++ filter_javascript/README.md
@@ -0,0 +1,3 @@
+ Filters Project
+
+Here we can filter the products through the name, company name. 
---
+++ filter_javascript/products.js
@@ -0,0 +1,98 @@
+const products = [
+  {
+    id: 'rec43w3ipXvP28vog',
+    title: 'high-back bench',
+    company: 'ikea',
+    image:
+      'https://th.bing.com/th/id/OIP.xsKWASCqMzH0d4Kk3bMyYQHaGt?w=192&h=180&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 9.99,
+  },
+  {
+    id: 'rec4f2RIftFCb7aHh',
+    title: 'albany table',
+    company: 'marcos',
+    image:
+      'https://th.bing.com/th/id/OIP.Qyuuwa8vwd8FwT-OUHikgAHaFl?w=247&h=186&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 79.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'accent chair',
+    company: 'caressa',
+    image:
+      'https://th.bing.com/th/id/OIP.xOEoZCYYoGewMsSVcf3RYgHaHa?w=181&h=181&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 25.99,
+  },
+  {
+    id: 'recBohCqQsot4Q4II',
+    title: 'wooden table',
+    company: 'caressa',
+    image:
+      'https://th.bing.com/th/id/OIP.ft-I43jPHDasuA5dkIbWQwHaF3?w=235&h=186&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 45.99,
+  },
+  {
+    id: 'recDG1JRZnbpRHpoy',
+    title: 'dining table',
+    company: 'caressa',
+    image:
+      'https://th.bing.com/th/id/OIP.PwZFZGBXfJpQ-w6Oz3Bt4AHaFY?w=271&h=197&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 6.99,
+  },
+  {
+    id: 'recNWGyP7kjFhSqw3',
+    title: 'sofa set',
+    company: 'liddy',
+    image:
+      'https://th.bing.com/th/id/OIP.xvZHh8rubtc7Et06GJmGBQHaF7?w=244&h=195&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 69.99,
+  },
+  {
+    id: 'recZEougL5bbY4AEx',
+    title: 'modern bookshelf',
+    company: 'marcos',
+    image:
+      'https://th.bing.com/th/id/OIP.yEVaF11sJvxLyd_NWBvqTgHaEo?w=298&h=186&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 8.99,
+  },
+  {
+    id: 'recjMK1jgTb2ld7sv',
+    title: 'emperor bed',
+    company: 'liddy',
+    image:
+      'https://dl.airtable.com/.attachments/10fdf29ae17f2d1f98770ae42584d021/82b9403f/product-6.jpg',
+    price: 21.99,
+  },
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    title: 'utopia sofa',
+    company: 'marcos',
+    image:
+      'https://th.bing.com/th/id/OIP.qqj7lUzWvtsWE-8ZRdO1kwHaE_?w=298&h=201&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 39.95,
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    title: 'entertainment center',
+    company: 'liddy',
+    image:
+      'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
+    price: 29.98,
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    title: 'albany sectional',
+    company: 'ikea',
+    image:
+      'https://th.bing.com/th/id/OIP.McmQUDA0CAzJkHePyPlkIQHaD6?w=320&h=180&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 10.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    title: 'leather sofa',
+    company: 'liddy',
+    image:
+      'https://th.bing.com/th/id/OIP.jyZs2_5Y415K5iHNLtXAAwHaFj?w=239&h=180&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+    price: 9.99,
+  },
+];
